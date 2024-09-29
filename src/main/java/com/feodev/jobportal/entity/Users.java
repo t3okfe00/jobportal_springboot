14d1039db0fad59a2fba342b57100adf60c7fb53
efe okyar <f.efe.okyar@gmail.com>
2024-09-29 19:08:42 +0300

    Users and UsersType entities are implemented

---
+++ src/main/java/com/feodev/jobportal/entity/Users.java
@@ -0,0 +1,104 @@
+package com.feodev.jobportal.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.usertype.UserType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+import java.util.Date;
+
+@Entity
+@Table(name="users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private boolean isActive;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="userTypeId",referencedColumnName = "userTypeId")
+    private UsersType userTypeId;
+
+    public Users() {
+    }
+
+    public Users(int userId, String email, String password, boolean isActive, Date registrationDate, UsersType userTypeId) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+        this.registrationDate = registrationDate;
+        this.userTypeId = userTypeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public UsersType getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public void setUserTypeId(UsersType userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
+                ", registrationDate=" + registrationDate +
+                ", userTypeId=" + userTypeId +
+                '}';
+    }
+}
